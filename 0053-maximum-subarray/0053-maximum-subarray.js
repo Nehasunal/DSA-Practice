747be43bc2e9e146a683a9335ac095029a085695
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2025-12-01 12:06:05 +0530

    Time: 4 ms (20.94%), Space: 65.7 MB (6.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,17 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    if(nums.length === 0) return 0;
+  let maxCount= nums[0]
+  let count = nums[0]
+  for(let i=1;i<nums.length;i++){
+     count = Math.max(nums[i]+count, nums[i])
+     maxCount = Math.max(count, maxCount)
+  }
 
-    let currentMax = nums[0]; // max sum ending at current index
-    let globalMax = nums[0];  // overall max sum
-
-    for(let i = 1; i < nums.length; i++){
-        // either extend previous subarray or start new from nums[i]
-        currentMax = Math.max(nums[i], nums[i] + currentMax);
-        globalMax = Math.max(globalMax, currentMax);
-    }
-
-    return globalMax;
+  return maxCount
 
 };
