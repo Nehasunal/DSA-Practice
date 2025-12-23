3b15516c606f6549d658e068368c7a4f19d4a455
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2025-12-23 22:20:26 +0530

    Time: 10 ms (81.77%), Space: 55.3 MB (89.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,39 +5,23 @@
  */
 var isAnagram = function(s, t) {
     let smap = new Map()
-    let tmap = new Map()
      if(s.length!== t.length){
         return false
      }
     for(let i=0;i<s.length;i++){
-        let count = 0
-       if(smap.has(s[i])){
-          count = smap.get(s[i])
-       }
-         count++
-          smap.set(s[i], count)
+        smap.set(s[i], (smap.get(s[i]) || 0)+1)
     }
 
     for(let j=0;j< t.length;j++){
-       if(!smap.has(t[j])){
+        if(!smap.has(t[j])){
 
-       return false
-       }
-          count = smap.get(t[j])
-          count--;
-          smap.set(t[j], count)
-          if(count === 0){
+                return false
+        } 
+          smap.set(t[j], smap.get(t[j]) - 1)
+          if(smap.get(t[j]) === 0){
             smap.delete(t[j])
           }
        
     }
-    // for(let [key, value] of smap){
-    //     if(!tmap.has(key)){
-    //         return false
-    //     }
-    //     if(value !== tmap.get(key)){
-    //         return false;
-    //     }
-    // }
     return true
 };
