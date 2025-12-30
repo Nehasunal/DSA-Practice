0835692e75d18d4dbd5ddc89eabc43ebca1cf348
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2025-12-30 21:20:24 +0530

    Time: 13 ms (7.59%), Space: 59.1 MB (7.01%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if(s.length!==t.length){
+        return false
+    }
+    let map=new Map()
+    // {
+    //     b: b,
+    //     a: a,
+    //     d: b,
+    //     c: a
+    // }\
+    //     
+    //     
+    // for(let i=0;i<s.length;i++){
+    //   if(map.has(s[i])){
+    //      if(map.get(s[i]) !== t[i]){
+    //         return false
+    //      }
+    //   } else if(map.has(t[i])){
+    //     if(map.get(t[i]) !== s[i]){
+    //         return false
+    //      }
+    //   } else{
+
+    //     map.set(s[i], t[i])
+    //   }
+    // }
+    for(let i=0;i<s.length;i++){
+      if(map.has(s[i])){
+         if(map.get(s[i]) !== t[i]){
+            return false
+         }
+      } else{       
+        const valueExists = [...map.values()].includes(t[i])
+        console.log(valueExists, t[i], s[i])
+        if(valueExists){
+            return false
+        }
+      }
+      map.set(s[i], t[i])
+    }
+    return true
+};
