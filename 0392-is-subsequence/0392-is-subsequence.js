a91df7fc5e6eaddf62b04aac3c853fbdb2b4f118
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-03-10 15:57:53 +0530

    Time: 1 ms (70.98%), Space: 54.2 MB (92.31%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -6,22 +6,12 @@
 var isSubsequence = function(s, t) {
     let i=0;
     let j=0;
-    let count = 0;
-    if(s === ""){
-        return true
-    }
     while(i<s.length && j<t.length){
-        while(s[i]!==t[j] && j<t.length){
-            j++
-        }
-        while(s[i] === t[j] && i<s.length && j<t.length){
+        if(s[i] === t[j]){
             i++
-            j++
-            count++
         }
-        if(count === s.length){
-            return true
-        }
+        j++
     }
-    return false
+
+    return i === s.length;
 };
