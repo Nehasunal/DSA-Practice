74a6b2838878e57305edfec26d8a97c19dfc4bfc
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-03-10 18:19:42 +0530

    Time: 4 ms (23.52%), Space: 58.2 MB (45.09%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let j=0;
+    let i=0;
+    let k=0;
+    if(chars.length <= 1){
+        return chars.length
+    }
+    while(i < chars.length && j<chars.length){
+        let count= 0;
+        while(chars[i] === chars[j]){
+           count++
+           j++
+        }
+        chars[k] = chars[i]
+        k++
+        if(count>1){
+            let str = count.toString();
+
+            for(let digit of str){
+                chars[k] = digit;
+                k++;
+            }
+        }
+        i=j
+
+    }
+    return k
+};
