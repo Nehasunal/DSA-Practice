f4cd82b1b0198135214d98ef1358bf14eba1bd0d
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-04-12 14:38:35 +0530

    Time: 101 ms (91.73%), Space: 77.7 MB (67.84%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -13,12 +13,13 @@
 var goodNodes = function(root) {
         let count = 0
         let max= root.val
-        function sums(root, max){
+        function dfs(root, max){
            if(!root) return 0
            if(max <= root.val) count++
            max= Math.max(root.val, max)
-           return sums(root.left, max) || sums(root.right, max)
+           dfs(root.left, max) 
+           dfs(root.right, max)
         }
-        sums(root, max)
+        dfs(root, max)
         return count
 };
