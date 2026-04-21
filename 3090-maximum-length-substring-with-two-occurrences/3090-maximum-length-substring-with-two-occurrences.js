bb855865a2f767fd7c6e9ce75d573cbfca909833
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-04-21 12:18:21 +0530

    Time: 3 ms (74.16%), Space: 55.5 MB (79.78%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.js
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.js
@@ -5,17 +5,14 @@
 var maximumLengthSubstring = function(s) {
     let left = 0
     let maxv = 0
-    let count = 0
     let map = new Map()
     for(let i =0;i < s.length; i++){
         map.set(s[i], (map.get(s[i])||0)+1)
-        count++
         while(map.has(s[i]) && map.get(s[i])>2){
            map.set(s[left], map.get(s[left])-1)
-           count--
            left++
         }
-        maxv = Math.max(maxv, count)
+        maxv = Math.max(maxv, i-left+1)
         
     }
     return maxv;
