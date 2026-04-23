4a47489c7670b3ba78c500d13e3af0fe22346352
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-04-23 18:36:00 +0530

    Time: 2 ms (74.11%), Space: 56.2 MB (78.57%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} customers
+ * @param {number[]} grumpy
+ * @param {number} minutes
+ * @return {number}
+ */
+var maxSatisfied = function(customers, grumpy, minutes) {
+    let sum = 0;
+    for(let i=0; i < customers.length;i++){
+        if(grumpy[i] === 0){
+            sum = sum + customers[i]
+        }
+    }
+    for(let i=0; i < minutes;i++){
+        if(grumpy[i] === 1){
+            sum = sum + customers[i]
+        }
+    }
+    let max =sum
+    for(let i=minutes; i < customers.length;i++){ 
+        if(grumpy[i-minutes] !== 0){
+            sum = sum - customers[i - minutes] 
+        }
+        if(grumpy[i] === 1){
+          sum = sum+customers[i]
+        }
+        max = Math.max(sum,max)
+    }
+    return max
+};
