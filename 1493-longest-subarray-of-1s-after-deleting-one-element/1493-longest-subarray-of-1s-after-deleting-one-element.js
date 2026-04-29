d494117c40839f412b3a30d488247abe035b38e5
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-04-29 11:36:04 +0530

    Time: 2 ms (66.02%), Space: 61.1 MB (26.97%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -2,18 +2,34 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var longestSubarray = function(nums) {
+//     let count = 0
+//     let max = 0
+//     let left =0
+//     for(let i=0;i<nums.length;i++){
+//         if(nums[i]===0) count++
+//         while(count>1){
+//             if(nums[left]===0) count--
+//             left++
+//         }
+//         max= Math.max(max, i-left)
+//     }
+//     if(count === 0) return nums.length-1
+//     return max
+// };
 var longestSubarray = function(nums) {
-    let count = 0
-    let max = 0
-    let left =0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===0) count++
-        while(count>1){
-            if(nums[left]===0) count--
-            left++
+    let left = 0;
+    let max_length = 0;
+    let last_zero = -1;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            left = last_zero + 1;
+            last_zero = right;
         }
-        max= Math.max(max, i-left)
+
+        max_length = Math.max(max_length, right - left);
     }
-    if(count === 0) return nums.length-1
-    return max
+
+    return max_length;    
 };
