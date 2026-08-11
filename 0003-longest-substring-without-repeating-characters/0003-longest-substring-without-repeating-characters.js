b49386617282d2f5b264e3207ec777b8f61a7c9b
Neha Sunal <50477274+Nehasunal@users.noreply.github.com>
2026-08-11 12:17:45 +0530

    Time: 143 ms (8.24%), Space: 63.8 MB (6.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,17 +5,31 @@
 var lengthOfLongestSubstring = function(s) {
   let map = new Map();
     let max = 0;
-    let left = 0;
+    let i = 0;
+    let j=0;
 
-    for (let right = 0; right < s.length; right++) {
-        let char = s[right];
+    // for (let right = 0; right < s.length; right++) {
+    //     let char = s[right];
 
-        if (map.has(char)) {
-            left = Math.max(map.get(char) + 1, left);
+    //     if (map.has(char)) {
+    //         left = Math.max(map.get(char) + 1, left);
+    //     }
+
+    //     map.set(char, right);
+    //     max = Math.max(max, right - left + 1);
+    // }
+    while(j < s.length){
+       while(map.has(s[j])){
+        map.set(s[i], map.get(s[i])-1)
+        if(map.get(s[i])===0){
+            map.delete(s[i])
         }
+        i++
+       }
+       map.set(s[j], (map.get(s[j]) || 0)+1)
 
-        map.set(char, right);
-        max = Math.max(max, right - left + 1);
+       max = Math.max(max, j-i+1)
+       j++
     }
 
     return max;
